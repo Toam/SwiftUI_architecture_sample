5362a5e4e203549f470231fab1816360bf13f4a2
Thomas Brelet <thomas.brelet@gmail.com>
2026-04-14 12:33:14 +0200

    Setup project

---
+++ SwiftUIArchitectureSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIArchitectureSample
+//
+//  Created by Thomas Brelet on 14/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIArchitectureSample/SwiftUIArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIArchitectureSampleApp.swift
+//  SwiftUIArchitectureSample
+//
+//  Created by Thomas Brelet on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
